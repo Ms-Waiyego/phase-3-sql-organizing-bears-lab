01e254a6ef589372eeb2253b773871cf5c705fe0
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-11-02 02:45:11 +0300

    completed lad

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Mr. Chocolate", 14 ,"M", "Turquoise" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Rowdy", 16 ,"F", "Beige" , "Happy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Tabitha", 15 ,"F", "Brown" , "Grumpy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Sergeant Brown", 20 ,"M", "Happy" , "Grumpy" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Melissa", 19 ,"F", "Blue" , "Clumsy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Grinch", 25 ,"M", "Yellow" , "Sad" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Wendy", 17 ,"F", "Purple" , "Happy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ( NULL,6 ,"M", "Blue" , "Melencholic" , true);
+
